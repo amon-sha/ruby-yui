d310e5c658deefe1665f065096625fdaac3e87ab
Amon Sha <amon.sha@gmail.com>
2017-10-06 17:33:38 +0300

    Defer YUI initialization

--- lib/yui/builder.rb
+++ lib/yui/builder.rb
@@ -1,11 +1,10 @@
 module Yui
   module Builder
-    FACTORY = Yui::YUI::widget_factory
-
-    FACTORY.methods.select { |meth| meth =~ /^create_/ }.each do |meth|
+    Yui::YWidgetFactory.instance_methods.each do |meth|
+      next unless meth =~ /^create_/
       define_method(meth.to_s.sub(/^create_/, '')) do |*args, **opts, &block|
         args.unshift(@__yui_builder_parent) unless meth =~ /_dialog/
-        widget = FACTORY.send(meth, *args)
+        widget = Yui::YUI::widget_factory.send(meth, *args)
         opts.each { |opt, value| widget[opt] = value }
         if block
           old_parent, @__yui_builder_parent = @__yui_builder_parent, widget
